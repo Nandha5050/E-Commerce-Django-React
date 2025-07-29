4170e50b3cc1b182ef3d451ee230675dd8764431
Nandha5050 <nandhasivannk@gmail.com>
2025-07-29 13:40:17 +0530

    Removed old Drfp folder

---
+++ server/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from Backend.views import Productview
+from rest_framework import routers
+
+route = routers.DefaultRouter()
+route.register('',Productview,basename='productview')
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/',include(route.urls)),
+    ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
